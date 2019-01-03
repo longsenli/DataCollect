982042a28636bbd08506546979ec106776192c48
unknown <zycczwf@sina.com>
2019-01-03 15:14:16 +0800

    clean

--- src/com/tnpy/datacollector/serial/DataView.java
+++ src/com/tnpy/datacollector/serial/DataView.java
@@ -8,13 +8,11 @@ import java.awt.Frame;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Label;
-import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.util.List;
-import java.util.TooManyListenersException;
 
 import javax.swing.JOptionPane;
 
